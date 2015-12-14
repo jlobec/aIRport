4662198113969f2986e89d9f54f70413ec689662
Brais <brais.miron@gmail.com>
2015-12-14 19:01:50 +0100

    Cambios en las dependencias de pom.xml
    Se ha creado la clase SimpleDemo como en los apuntes para probar el
    funcionamiento (es una chapuza)

--- src/main/java/es/fic/udc/riws/airport/home/HomeController.java
+++ src/main/java/es/fic/udc/riws/airport/home/HomeController.java
@@ -1,15 +1,21 @@
 package es.fic.udc.riws.airport.home;
 
+import java.io.IOException;
 import java.security.Principal;
 
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import es.fic.udc.riws.airport.simpledemo.SimpleDemo;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Principal principal) {
+	public String index(Principal principal) throws IOException, ParseException {
+		SimpleDemo demo = new SimpleDemo();
+		demo.index();
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
