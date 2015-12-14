025933d18c5eb9b5774b71acbb03637f28fceeb9
Brais Miron <Brais@MacBook-Pro-de-Brais.local>
2015-12-14 18:19:21 +0100

    Creacion de arquetipo web (sin Lucene)

---
+++ src/main/java/es/fic/udc/riws/airport/Application.java
@@ -0,0 +1,3 @@
+package es.fic.udc.riws.airport;
+
+public interface Application {}
---
+++ src/test/java/es/fic/udc/riws/airport/config/NoCsrfSecurityConfig.java
@@ -0,0 +1,15 @@
+package es.fic.udc.riws.airport.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class NoCsrfSecurityConfig extends SecurityConfig {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
+
+}
